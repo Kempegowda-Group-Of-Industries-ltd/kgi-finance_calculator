8957d6b8132dfc1ef6b8748a57c856b278c0f38c
KempeGowda Group Of Industries Ltd <kempegowdagroupofindustriesltd@gmail.com>
2024-09-14 01:09:48 -0400

    app.py

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ from calculators.loan_calculator import loan_calculator
 from calculators.investment_calculator import investment_calculator
 from calculators.budget_calculator import budget_calculator
 from calculators.retirement_calculator import retirement_calculator
+from calculators.mortgage_calculator import mortgage_calculator  # New
+from calculators.auto_loan_calculator import auto_loan_calculator  # New
+from calculators.compound_interest_calculator import compound_interest_calculator  # New
 
 # Sidebar for navigation
 st.sidebar.title("Finance Calculators & Simulators")
 option = st.sidebar.selectbox("Choose a calculator:", 
-                             ["Loan Calculator", "Investment Growth", "Budget Tracker", "Retirement Calculator"])
+                             ["Loan Calculator", 
+                              "Investment Growth", 
+                              "Budget Tracker", 
+                              "Retirement Calculator", 
+                              "Mortgage Calculator",  # New
+                              "Auto Loan Calculator",  # New
+                              "Compound Interest Calculator"])  # New
 
 # Render the appropriate calculator based on user selection
 if option == "Loan Calculator":
@@ -18,3 +27,9 @@ elif option == "Budget Tracker":
     budget_calculator()
 elif option == "Retirement Calculator":
     retirement_calculator()
+elif option == "Mortgage Calculator":
+    mortgage_calculator()  # New
+elif option == "Auto Loan Calculator":
+    auto_loan_calculator()  # New
+elif option == "Compound Interest Calculator":
+    compound_interest_calculator()  # New
