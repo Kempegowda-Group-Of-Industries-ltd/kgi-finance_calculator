a4e29f6056864a2ea3e76649dce72d4d7f14d33c
KempeGowda Group Of Industries Ltd <kempegowdagroupofindustriesltd@gmail.com>
2024-09-14 01:32:49 -0400

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import streamlit as st
 from calculators.loan_calculator import loan_calculator
 from calculators.investment_calculator import investment_calculator
 from calculators.budget_calculator import budget_calculator
 from calculators.retirement_calculator import retirement_calculator
-from calculators.mortgage_calculator import mortgage_calculator  # New
-from calculators.auto_loan_calculator import auto_loan_calculator  # New
-from calculators.compound_interest_calculator import compound_interest_calculator  # New
+from calculators.mortgage_calculator import mortgage_calculator
+from calculators.auto_loan_calculator import auto_loan_calculator
+from calculators.compound_interest_calculator import compound_interest_calculator
 
 # Sidebar for navigation
 st.sidebar.title("Finance Calculators & Simulators")
@@ -13,10 +12,10 @@ option = st.sidebar.selectbox("Choose a calculator:",
                              ["Loan Calculator", 
                               "Investment Growth", 
                               "Budget Tracker", 
-                              "Retirement Calculator", 
-                              "Mortgage Calculator",  # New
-                              "Auto Loan Calculator",  # New
-                              "Compound Interest Calculator"])  # New
+                              "Retirement Calculator",
+                              "Mortgage Calculator",
+                              "Auto Loan Calculator",
+                              "Compound Interest Calculator"])
 
 # Render the appropriate calculator based on user selection
 if option == "Loan Calculator":
@@ -28,8 +27,8 @@ elif option == "Budget Tracker":
 elif option == "Retirement Calculator":
     retirement_calculator()
 elif option == "Mortgage Calculator":
-    mortgage_calculator()  # New
+    mortgage_calculator()
 elif option == "Auto Loan Calculator":
-    auto_loan_calculator()  # New
+    auto_loan_calculator()
 elif option == "Compound Interest Calculator":
-    compound_interest_calculator()  # New
+    compound_interest_calculator()
